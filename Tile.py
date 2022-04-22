e654d1bc3b878bfa0dac3ebbb9ab9138908b4524
tzada8 <tzada@uwaterloo.ca>
2022-04-21 20:19:52 -0400

    refactor(Tile): Added Frame and Label elements directly in Tile class

--- Tile.py
+++ Tile.py
@@ -1,6 +1,27 @@
+from tkinter import Frame, Label
+from custom_tkinter.CustomFrame import CustomFrame
+
+
 class Tile:
     FONT = ("Comic Sans", 18)
+    REL_FRAME_SIZE = 1 / 9  # 0.111.
+    REL_LABEL_LOC = 1 / 10  # 0.1.
+    REL_LABEL_SIZE = 4 / 5  # 0.8.
 
-    def __init__(self, border, label):
-        self.border = border  # Holds frame with green/red border
-        self.label = label  # Holds value of tile in center of frame
+    def __init__(self, location, value):
+        # Frame for green/red border.
+        self.border = CustomFrame(location)
+
+        # Value in center of frame.
+        self.label = Label(self.border, text=value, font=self.FONT, bg="white", fg="black")
+
+    # Updates Tile's border colour and label.
+    def update(self, colour, value):
+        self.border["highlightbackground"] = colour
+        self.border["highlightcolor"] = colour
+        self.label["text"] = value
+
+    # Places tile.
+    def place(self, x, y):
+        self.border.place(x, y, self.REL_FRAME_SIZE, self.REL_FRAME_SIZE)
+        self.label.place(relx=self.REL_LABEL_LOC, rely=self.REL_LABEL_LOC, relwidth=self.REL_LABEL_SIZE, relheight=self.REL_LABEL_SIZE)
