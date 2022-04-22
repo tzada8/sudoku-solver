30344f62de12fa9c695f848e7331fa02736c3a76
tzada8 <tzada@uwaterloo.ca>
2022-04-22 19:40:40 -0400

    refactor(Tile): Updated Tile to use CustomTkinter elements

--- Tile.py
+++ Tile.py
@@ -1,22 +1,19 @@
-from tkinter import Frame, Label
 from custom_tkinter.CustomFrame import CustomFrame
+from custom_tkinter.CustomLabel import CustomLabel
 from custom_tkinter.CustomCanvas import CustomCanvas
-from colours import BASE
+from Board import Board
 
 
 class Tile:
-    SIZE = CustomCanvas.SIZE / 9  # Each Tile is approximately 41.55x41.55.
-    FONT = ("Comic Sans", 18)
-    REL_FRAME_SIZE = 1 / 9  # 0.111.
-    REL_LABEL_LOC = 1 / 10  # 0.1.
-    REL_LABEL_SIZE = 4 / 5  # 0.8.
+    SIZE = CustomCanvas.SIZE / Board.SIZE  # Each Tile is approximately 41.55x41.55.
+    FRAME_SIZE = 1 / Board.SIZE  # 0.111.
+    LABEL_LOC = 1 / 10  # 0.1.
+    LABEL_SIZE = 4 / 5  # 0.8.
 
+    # Each tile has a frame for its border and a label centered inside the frame.
     def __init__(self, location, value):
-        # Frame for green/red border.
         self.border = CustomFrame(location)
-
-        # Value in center of frame.
-        self.label = Label(self.border, text=value, font=self.FONT, bg=BASE["WHITE"], fg=BASE["BLACK"])
+        self.label = CustomLabel(self.border, value)
 
     # Updates Tile's border colour and label.
     def update(self, colour, value):
@@ -26,5 +23,5 @@ class Tile:
 
     # Places tile.
     def place(self, x, y):
-        self.border.place(x, y, self.REL_FRAME_SIZE, self.REL_FRAME_SIZE)
-        self.label.place(relx=self.REL_LABEL_LOC, rely=self.REL_LABEL_LOC, relwidth=self.REL_LABEL_SIZE, relheight=self.REL_LABEL_SIZE)
+        self.border.place(x, y, self.FRAME_SIZE, self.FRAME_SIZE)
+        self.label.place(relx=self.LABEL_LOC, rely=self.LABEL_LOC, relwidth=self.LABEL_SIZE, relheight=self.LABEL_SIZE)
