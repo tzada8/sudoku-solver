d91d24b40e152494eb709db43713d466cea2ac61
tzada8 <tzada@uwaterloo.ca>
2022-04-22 20:08:31 -0400

    test(Board): Added test cases for solve method of Board

--- tests/TestBoard.py
+++ tests/TestBoard.py
@@ -1,4 +1,3 @@
-import pdb
 import unittest
 from Board import Board
 
@@ -165,6 +164,37 @@ class TestBoard(unittest.TestCase):
 
     def test_is_fully_valid_with_valid_board(self):
         self.assertTrue(Board(self.board_full).is_fully_valid())
+
+    def test_solve_with_solvable_board(self):
+        expected = [
+            [5, 3, 4, 6, 7, 8, 9, 1, 2],
+            [6, 7, 2, 1, 9, 5, 3, 4, 8],
+            [1, 9, 8, 3, 4, 2, 5, 6, 7],
+            [8, 5, 9, 7, 6, 1, 4, 2, 3],
+            [4, 2, 6, 8, 5, 3, 7, 9, 1],
+            [7, 1, 3, 9, 2, 4, 8, 5, 6],
+            [9, 6, 1, 5, 3, 7, 2, 8, 4],
+            [2, 8, 7, 4, 1, 9, 6, 3, 5],
+            [3, 4, 5, 2, 8, 6, 1, 7, 9]
+        ]
+        board = Board(self.board)
+        self.assertTrue(board.solve(None, False))
+        self.assertEqual(board.get_bo(), expected)
+
+    def test_solve_with_unsolvable_board(self):
+        unsolvable_board = [
+            [5, 1, 6, 8, 4, 9, 7, 3, 2],
+            [3, 0, 7, 6, 0, 5, 0, 0, 0],
+            [8, 0, 9, 7, 0, 0, 0, 6, 5],
+            [1, 3, 5, 0, 6, 0, 9, 0, 7],
+            [4, 7, 2, 5, 9, 1, 0, 0, 6],
+            [9, 6, 8, 3, 7, 0, 0, 5, 0],
+            [2, 5, 3, 1, 8, 6, 0, 7, 4],
+            [6, 8, 4, 2, 0, 7, 5, 0, 0],
+            [7, 9, 1, 0, 5, 0, 6, 0, 8]
+        ]
+        board = Board(unsolvable_board)
+        self.assertFalse(board.solve(None, False))
 
     def test_is_square_with_not_squared_board(self):
         self.assertFalse(Board.is_square(self.board_1x3))
