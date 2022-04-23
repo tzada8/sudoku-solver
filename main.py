593e953dec030a92e273eb76c0384cdaed4e9a78
tzada8 <tzada@uwaterloo.ca>
2022-04-22 20:27:24 -0400

    refactor(Comments): Updated comments to more accurately represent functionality

--- main.py
+++ main.py
@@ -15,16 +15,19 @@ from board_interface.BoardCreate import BoardCreate
 # Prompts to visually see solution as it's being solved.
 def prompt_to_solve(board, board_view):
     if PopUp.question() == PopUp.CONFIRM:
-        if not board.solve(board_view, True):
-            PopUp.info()
+        __no_solution(board, board_view, True)
         board_view.refresh(board)
     else:
-        if not board.solve(board_view, False):
-            PopUp.info()
+        __no_solution(board, board_view, False)
         board_view.place_known_values(board)
 
 
-# User inputs values into entry boxes, such that they can solve any puzzle they want
+def __no_solution(board, board_view, is_visual):
+    if not board.solve(board_view, is_visual):
+        PopUp.info()
+
+
+# Window to create custom board to solve.
 def custom_board(board: Board, board_view: BoardView):
     top_level = CustomTopLevel("Create Sudoku Board")
     board_create = BoardCreate(top_level)
@@ -32,6 +35,7 @@ def custom_board(board: Board, board_view: BoardView):
     instructions = CustomLabel(top_level, "Fill in the 9x9 grid with digits from 1 to 9", FONT["BUTTON"], WINDOW_BG)
     instructions.place(rely=0.015, relwidth=1)
 
+    # Button to create custom board.
     confirm_btn = CustomButton(top_level, "Confirm", lambda: board_create.confirm(board, board_view))
     confirm_btn.place(0.375, 0.25)
 
@@ -47,7 +51,7 @@ def main():
     solve_btn = CustomButton(root, "Solve", lambda: prompt_to_solve(board, board_view))
     solve_btn.place(0.09, 0.2)
 
-    # Button to create custom board.
+    # Button to open custom board window.
     create_board_btn = CustomButton(root, "Create", (lambda: custom_board(board, board_view)))
     create_board_btn.place(0.715, 0.2)
 
